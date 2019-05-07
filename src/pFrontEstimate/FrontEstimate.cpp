5af7dafe999764c86e683d3db4e62ba4838121c1
bastanfi <bastanfi@mit.edu>
2019-05-07 15:47:30 -0400

    ds

--- src/pFrontEstimate/FrontEstimate.cpp
+++ src/pFrontEstimate/FrontEstimate.cpp
@@ -373,7 +373,7 @@ bool CFrontEstimate::OnNewMail(MOOSMSG_LIST &NewMail)
     size_t n = std::count(value.begin(), value.end(), ':');
     for(int count=1; count !=n; count++){
       string m_new = biteString(value,':');
-      if(m_new = "")
+      if(m_new == "")
         continue;
       string vname = "me";
       string temp = tokStringParse(m_new,"temp",';','=');
